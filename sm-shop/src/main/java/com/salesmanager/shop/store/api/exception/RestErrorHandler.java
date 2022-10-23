81db863827f528d51adbca5e3447e013d6cb62ef
volodymyrd <volodymyrdotsenko@gmail.com>
2022-10-23 18:13:29 +0200

    address nullpointer exception.

--- sm-shop/src/main/java/com/salesmanager/shop/store/api/exception/RestErrorHandler.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/api/exception/RestErrorHandler.java
@@ -1,24 +1,20 @@
 package com.salesmanager.shop.store.api.exception;
 
-import java.util.Objects;
-import java.util.Optional;
-
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+import java.util.Optional;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice({"com.salesmanager.shop.store.api"})
 public class RestErrorHandler {
-  
+
     private static final Logger log = LoggerFactory.getLogger(RestErrorHandler.class);
 
     @RequestMapping(produces = "application/json")
@@ -28,12 +24,14 @@ public class RestErrorHandler {
         log.error(exception.getMessage(), exception);
         Objects.requireNonNull(exception.getCause());
         Throwable rootCause = exception.getCause();
-        while (rootCause.getCause() != null && rootCause.getCause() != rootCause) {
+        while (rootCause != null && rootCause.getCause() != null && rootCause.getCause() != rootCause) {
             rootCause = rootCause.getCause();
         }
-        ErrorEntity errorEntity = createErrorEntity("500", exception.getMessage(),
-        		rootCause.getMessage());
-        return errorEntity;
+        if (rootCause != null) {
+            return createErrorEntity("500", exception.getMessage(),
+                    rootCause.getMessage());
+        }
+        return createErrorEntity("500", exception.getMessage(), "");
     }
 
 
@@ -46,12 +44,19 @@ public class RestErrorHandler {
     public @ResponseBody ErrorEntity handleServiceException(ServiceRuntimeException exception) {
         log.error(exception.getErrorMessage(), exception);
         Throwable rootCause = exception.getCause();
-        while (rootCause.getCause() != null && rootCause.getCause() != rootCause) {
+        while (rootCause != null && rootCause.getCause() != null && rootCause.getCause() != rootCause) {
             rootCause = rootCause.getCause();
         }
-        ErrorEntity errorEntity = createErrorEntity(exception.getErrorCode()!=null?exception.getErrorCode():"500", exception.getErrorMessage(),
-        		rootCause.getMessage());
-        return errorEntity;
+        if (rootCause != null) {
+            return createErrorEntity(
+                    exception.getErrorCode() != null ? exception.getErrorCode() : "500",
+                    exception.getErrorMessage(),
+                    rootCause.getMessage());
+        }
+        return createErrorEntity(
+                exception.getErrorCode() != null ? exception.getErrorCode() : "500",
+                exception.getErrorMessage(),
+                exception.getMessage());
     }
 
     @RequestMapping(produces = "application/json")
@@ -60,7 +65,7 @@ public class RestErrorHandler {
     public @ResponseBody ErrorEntity handleServiceException(ConversionRuntimeException exception) {
         log.error(exception.getErrorMessage(), exception);
         ErrorEntity errorEntity = createErrorEntity(exception.getErrorCode(), exception.getErrorMessage(),
-            exception.getLocalizedMessage());
+                exception.getLocalizedMessage());
         return errorEntity;
     }
 
@@ -74,7 +79,7 @@ public class RestErrorHandler {
                 exception.getLocalizedMessage());
         return errorEntity;
     }
-    
+
     @RequestMapping(produces = "application/json")
     @ExceptionHandler(UnauthorizedException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
@@ -102,9 +107,9 @@ public class RestErrorHandler {
         Optional.ofNullable(errorCode)
                 .ifPresent(errorEntity::setErrorCode);
 
-        String resultMessage = (message != null && detailMessage !=null)  ? new StringBuilder().append(message).append(", ").append(detailMessage).toString() : detailMessage;
-        if(StringUtils.isBlank(resultMessage)) {
-        	resultMessage = message;
+        String resultMessage = (message != null && detailMessage != null) ? new StringBuilder().append(message).append(", ").append(detailMessage).toString() : detailMessage;
+        if (StringUtils.isBlank(resultMessage)) {
+            resultMessage = message;
         }
         Optional.ofNullable(resultMessage)
                 .ifPresent(errorEntity::setMessage);
